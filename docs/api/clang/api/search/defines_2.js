acefc16d26a5ffc56a7d011af751c2f665578bbd
Paul Lorenz <paul.lorenz@netfoundry.io>
2020-03-09 21:36:02 -0400

    Update generated files

--- docs/api/clang/api/search/defines_2.js
+++ docs/api/clang/api/search/defines_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ziti_5fcan_5fbind_86',['ZITI_CAN_BIND',['../ziti_8h.html#a7ad10991d93ba9fbdd3724a7eb64fb76',1,'ziti.h']]],
-  ['ziti_5fcan_5fdial_87',['ZITI_CAN_DIAL',['../ziti_8h.html#a01d00c5d71bfb04e1ded0c3f182f5cc3',1,'ziti.h']]],
-  ['ziti_5ferrors_88',['ZITI_ERRORS',['../errors_8h.html#ac40dbb62a3442640d8ea56b9169ac092',1,'errors.h']]]
+  ['ziti_5fcan_5fbind',['ZITI_CAN_BIND',['../ziti_8h.html#a7ad10991d93ba9fbdd3724a7eb64fb76',1,'ziti.h']]],
+  ['ziti_5fcan_5fdial',['ZITI_CAN_DIAL',['../ziti_8h.html#a01d00c5d71bfb04e1ded0c3f182f5cc3',1,'ziti.h']]],
+  ['ziti_5ferrors',['ZITI_ERRORS',['../errors_8h.html#ac40dbb62a3442640d8ea56b9169ac092',1,'errors.h']]]
 ];
