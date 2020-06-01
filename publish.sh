7e8860417749f83d92046e9c7553ba532780d34d
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-01 16:01:14 -0400

    don't fail the build when it's not the master branch

--- publish.sh
+++ publish.sh
@@ -5,14 +5,15 @@ gitbranch=$(git rev-parse --abbrev-ref HEAD)
 if [ "${gitbranch}" == "master" ]
 then
   echo on master branch - publish can proceed
+
+  ./gendoc.sh docs
+
+  ziti-ci configure-git
+  git add docs docfx_project/ziti-*
+  git commit -m "[ci skip] publish docs from travis"
+  git push
+
 else
-  echo ========= FAIL - cannot publish from branch that is not master : ${gitbranch}
-  exit 1
+  echo ========= cannot publish from branch that is not master : ${gitbranch}
+  echo ========= publish considered successful though no op
 fi
-
-./gendoc.sh docs
-
-ziti-ci configure-git
-git add docs docfx_project/ziti-*
-git commit -m "[ci skip] publish docs from travis"
-git push
