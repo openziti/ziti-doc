83be7c27198821901e22a094bf66338361e9ef11
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-01 15:24:28 -0400

    add a check for the branch in the publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+gitbranch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "${gitbranch}" == "master" ]
+then
+  echo on master branch - publish can proceed
+else
+  echo ========= FAIL - cannot publish from branch that is not master : ${gitbranch}
+  exit 1
+fi
+
 ./gendoc.sh docs
 
 ziti-ci configure-git
