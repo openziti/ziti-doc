41c6a19a10bb9d101575f239bf1f901b778850b2
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-02 23:37:05 -0400

    update publish again

--- publish.sh
+++ publish.sh
@@ -51,6 +51,7 @@ then
     echo changing git repo from https to git so that we can push...
     ../changeToSsh.sh
   fi
+  echo "0.0" > version
   ziti-ci configure-git
   git diff-index --quiet HEAD || git commit -m "[ci skip] publish docs from travis"
   git diff-index --quiet HEAD || git push
