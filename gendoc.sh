5a0b71315aee15342a72412bf7f456635a1e4944
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-06-03 10:56:50 -0400

    update gendoc to rm the output first (again) and update the logo

--- gendoc.sh
+++ gendoc.sh
@@ -52,6 +52,10 @@ else
   DOC_ROOT=$1
 fi
 
+# specifically using ../ziti-doc just to remove any chance to rm something unintended
+echo removing previous build at: rm $(pwd)/../ziti-doc/${DOC_ROOT}
+rm -r $(pwd)/../ziti-doc/${DOC_ROOT}
+
 pushd docfx_project
 docfx build
 popd
