d8768adbc60220ac888cec32c011abf6fbb95d89
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-11-16 16:50:11 -0500

    next commit

--- publish.sh
+++ publish.sh
@@ -63,9 +63,15 @@ then
     echo "${GH_KEY} DID NOT exist???"
   fi
 
-  echo "chmod'ing ${pub_script_root}/github_deploy_key to 600"
-  chmod 600 ${pub_script_root}/github_deploy_key
-  ssh -Tv -i ${pub_script_root}/github_deploy_key git@github.com
+  #echo "chmod'ing ${pub_script_root}/github_deploy_key to 600"
+  #chmod 600 ${pub_script_root}/github_deploy_key
+  #ssh -Tv -i ${pub_script_root}/github_deploy_key git@github.com
+  cp ${pub_script_root}/github_deploy_key ~/.ssh/id_rsa
+  chmod 600 ~/.ssh/id_rsa
+  echo "checking id_rsa exists:"
+  ls -l ~/.ssh/id_rsa
+
+  ssh -Tv git@github.com
 
   echo __________________________________________________________________________
   git config user.name ziti-ci
