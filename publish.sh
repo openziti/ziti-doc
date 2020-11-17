d0385a11d370cd87dd259a5792577d55c11ee5c6
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-11-17 16:37:01 -0500

    rearrange commands again

--- publish.sh
+++ publish.sh
@@ -80,14 +80,14 @@ then
 
   popd
   echo __________________________________________________________________________
+  git config user.name ziti-ci
+  git config user.email ziti-ci@netfoundry.io
+  git config core.sshCommand "ssh -i ${pub_script_root}/github_deploy_key"
   if [[ "$(git config --get remote.origin.url | cut -b1-3)" == "htt" ]]; then
     echo changing git repo from https to git so that we can push...
     ${pub_script_root}/changeToSsh.sh
   fi
 
-  git config user.name ziti-ci
-  git config user.email ziti-ci@netfoundry.io
-  git config core.sshCommand "ssh -i ${pub_script_root}/github_deploy_key"
   git diff-index --quiet HEAD || git commit -m "[ci skip] update ziti-doc submodules from CI" && git push
 else
   echo ========= cannot publish from branch that is not master : ${GIT_BRANCH}
