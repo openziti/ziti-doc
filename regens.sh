f041732507047379dbb2cb457e0cd6fef2df1b4f
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2022-08-04 11:50:54 -0400

    finish commenting

--- regens.sh
+++ regens.sh
@@ -6,7 +6,7 @@
 set -e -u -o pipefail
 
 # gendoc.sh will fail unless /ziti-doc is marked safe because it's owned by a
-# different EUID when running in a container
+# different EUID when running in a container and the Git working copy is not clean
 #git config --global --add safe.directory /ziti-doc
 
 # docfx must bind all interfaces inside the container or the Docker host will
