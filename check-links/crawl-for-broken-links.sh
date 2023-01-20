bb4e3ea899469b6ecf50c996775beacd5f7ae212
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-01-20 17:15:51 -0500

    match the broken link crawler script params to the CI job

--- check-links/crawl-for-broken-links.sh
+++ check-links/crawl-for-broken-links.sh
@@ -12,11 +12,15 @@ fi
 
 case "$SERVER" in
     *)
-        EXCLUDE='https://(EXWPKK5PV4-dsn.algolia.net|www.googletagmanager.com|mermaid-js.github.io|mermaid.live|play.google.com/store/apps/details\?id=io\.netfoundry\.ziti\.tunnel|apps.apple.com/|www.reddit.com/r/openziti$|.*\.ziti[^\.]+$)|#docusaurus_skipToContent_fallback$'
+        EXCLUDE='https://(EXWPKK5PV4-dsn.algolia.net|www.googletagmanager.com|mermaid-js.github.io|mermaid.live|play.google.com/|apps.apple.com/|www.reddit.com/r/openziti)|(#docusaurus_skipToContent_fallback|\.ziti/?)$'
     ;;
 esac
 
 docker run --rm --network=host raviqqe/muffet "${SERVER}" \
     --color=always \
+    --buffer-size=8192 \
+    --max-connections-per-host=5 \
+    --header=User-Agent:curl/7.54.0 \
+    --timeout=20 \
     --exclude="$EXCLUDE" \
     "${@}"
