d471c5b5fb42544f06927083dfdc3e24474455f5
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2023-08-02 08:16:43 -0400

    add alt text to logo

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -283,7 +283,7 @@ const config = {
         title: '',
         hideOnScroll: false,
         logo: {
-          alt: 'OpenZiti Logo',
+          alt: 'The OpenZiti logo, an open source zero trust overlay network overlay',
           src: 'img/ziti-logo-dark.svg',
           srcDark: 'img/ziti-logo-light.svg',
         },
