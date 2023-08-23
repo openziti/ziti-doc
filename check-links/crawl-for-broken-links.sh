047da298c803672a0d297731202c89c2a6ce4716
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-08-23 13:13:20 -0400

    stop checking garbage WP links

--- check-links/crawl-for-broken-links.sh
+++ check-links/crawl-for-broken-links.sh
@@ -32,6 +32,9 @@ www\.reddit\.com/r/openziti
 .*\.zitik8s
 .*\.svc
 twitter\.com/(OpenZiggy|OpenZiti)
+landing.openziti.io/xmlrpc.php?rsd
+landing.openziti.io/wp-includes/wlwmanifest.xml
+fonts.gstatic.com/
 EOF
 # github\.com/.*/releases/latest/download
 
