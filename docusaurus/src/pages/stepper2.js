6f6d3facecfa202c0c279cf6ba2b09e914d68efe
Jeremy Tellier <jeremy.tellier@netfoundry.io>
2023-09-07 09:15:30 -0400

    Remove unnecessary test files

--- docusaurus/src/pages/stepper2.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import WizardLocalNoDocker from "../components/WizardLocalNoDocker";
-
-
-export default function Stepper() {
-    return (
-        <WizardLocalNoDocker title="Everything Local - No Docker"></WizardLocalNoDocker>
-    )
-}
