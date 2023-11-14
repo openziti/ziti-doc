647f158db3b3de8bb29b53f8412483b558a6fd37
Geoff Berl <geoff.berl@netfoundry.io>
2023-11-14 10:50:03 -0500

    Adding dependency

--- docusaurus/src/pages/appetizer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AsciinemaWidget from '../../src/components/AsciinemaWidget';
-
-function App() {
-    return <>
-        <AsciinemaWidget fit={false} src="/appetizer.cast" rows={20} loop={true} autoplay={1} preload={true} />
-    </>;
-}
-export default App;
