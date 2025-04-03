0a1bb39e6af178fdc2ec8522516cec8a2f3358ba
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2025-04-03 18:55:05 -0400

    start organizing deployment docs

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -60,44 +60,45 @@ const config: Config = {
       '@docusaurus/plugin-client-redirects',
       {
         createRedirects: path => {
-          if ( path.startsWith("/docs/guides/topologies/gateway/") ) {
-            return [path.replace("/docs/guides/topologies/gateway/","/docs/guides/local-gateway/")];
-          }
-          if ( path.startsWith("/docs/guides/deployments/kubernetes/") ) {
-            return [path.replace("/docs/guides/deployments/kubernetes/","/docs/guides/kubernetes/hosting/")];
-          }
-          if ( path.startsWith("/docs/reference/tunnelers/kubernetes/") ) {
-            return [path.replace("/docs/reference/tunnelers/kubernetes/","/docs/guides/kubernetes/workload-tunneling/")];
-          }
-          if ( path.startsWith("/docs/guides/deployments/") ) {
-            return [
-              path.replace("/docs/guides/deployments/","/docs/reference/deployments/"),
-            ];
-          }
-          if ( path.startsWith("/docs/reference/developer/api/") ) {                       // for each existing page
-            return [
-              path.replace("/docs/reference/developer/api/","/api/"),                      // return a "from" redirect for each old path
-              path.replace("/docs/reference/developer/api/","/api/rest/"),
-              path.replace("/docs/reference/developer/api/","/api/rest/edge-apis/")
-            ];
-          }
-          if ( path.startsWith("/docs/learn/quickstarts/") ) {
-            return [path.replace("/docs/learn/quickstarts/","/docs/quickstarts/")];
-          }
-          if ( path.startsWith("/docs/learn/core-concepts/zero-trust-models/") ) {
-            return [
-              path.replace("/docs/learn/core-concepts/zero-trust-models/","/docs/deployment-architecture/"),
-              path.replace("/docs/learn/core-concepts/zero-trust-models/","/docs/core-concepts/zero-trust-models/")
-            ];
-          }
-          if ( path.startsWith("/docs/learn/core-concepts/") ) {
-            return [path.replace("/docs/learn/core-concepts/","/docs/core-concepts/")];
-          }
-          if ( path.startsWith("/docs/learn/introduction/") ) {
-            return [path.replace("/docs/learn/introduction/","/docs/introduction/")];
-          }
+          // if ( path.startsWith("/docs/guides/topologies/gateway/") ) {
+          //   return [path.replace("/docs/guides/topologies/gateway/","/docs/guides/local-gateway/")];
+          // }
+          // if ( path.startsWith("/docs/guides/deployments/kubernetes/") ) {
+          //   return [path.replace("/docs/guides/deployments/kubernetes/","/docs/guides/kubernetes/hosting/")];
+          // }
+          // if ( path.startsWith("/docs/reference/tunnelers/kubernetes/") ) {
+          //   return [path.replace("/docs/reference/tunnelers/kubernetes/","/docs/guides/kubernetes/workload-tunneling/")];
+          // }
+          // if ( path.startsWith("/docs/guides/deployments/") ) {
+          //   return [
+          //     path.replace("/docs/guides/deployments/","/docs/reference/deployments/"),
+          //   ];
+          // }
+          // if ( path.startsWith("/docs/reference/developer/api/") ) {                       // for each existing page
+          //   return [
+          //     path.replace("/docs/reference/developer/api/","/api/"),                      // return a "from" redirect for each old path
+          //     path.replace("/docs/reference/developer/api/","/api/rest/"),
+          //     path.replace("/docs/reference/developer/api/","/api/rest/edge-apis/")
+          //   ];
+          // }
+          // if ( path.startsWith("/docs/learn/quickstarts/") ) {
+          //   return [path.replace("/docs/learn/quickstarts/","/docs/quickstarts/")];
+          // }
+          // if ( path.startsWith("/docs/learn/core-concepts/zero-trust-models/") ) {
+          //   return [
+          //     path.replace("/docs/learn/core-concepts/zero-trust-models/","/docs/deployment-architecture/"),
+          //     path.replace("/docs/learn/core-concepts/zero-trust-models/","/docs/core-concepts/zero-trust-models/")
+          //   ];
+          // }
+          // if ( path.startsWith("/docs/learn/core-concepts/") ) {
+          //   return [path.replace("/docs/learn/core-concepts/","/docs/core-concepts/")];
+          // }
+          // if ( path.startsWith("/docs/learn/introduction/") ) {
+          //   return [path.replace("/docs/learn/introduction/","/docs/introduction/")];
+          // }
           return undefined;
         },
+<<<<<<< HEAD:docusaurus/docusaurus.config.ts
         redirects: [
           {
             to: '/docs/category/deployments',
@@ -245,6 +246,155 @@ const config: Config = {
           }
         ],
       } satisfies ClientRedirectsOptions,
+=======
+        redirects: [],
+        //   {
+        //     to: '/docs/category/deployments',
+        //     from: ['/docs/reference/deployments']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/linux/controller/deploy',
+        //     from: ['/docs/reference/deployments/controller']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/linux/router/cli-mgmt',
+        //     from: ['/docs/reference/deployments/router/cli-mgmt']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/linux/router/deploy',
+        //     from: ['/docs/reference/deployments/router/deployment']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/linux/router/router-configuration',
+        //     from: ['/docs/reference/deployments/router/router-configuration']
+        //   },
+        //   {
+        //     to: '/docs/reference/tunnelers/docker',
+        //     from: ['/docs/reference/tunnelers/linux/container']
+        //   },
+        //   {
+        //     to: '/docs/category/core-concepts',
+        //     from: ['/docs/learn/core-concepts']
+        //   },
+        //   {
+        //     to: '/docs/reference/tunnelers/nginx',
+        //     from: ['/docs/guides/securing-apis/aks-api-with-nginx-ziti-module']
+        //   },
+        //   {
+        //     to: '/docs/category/cloud',
+        //     from: ['/docs/guides/Public_Cloud_Deployment']
+        //   },
+        //   {
+        //     to: '/docs/guides/topologies/services',
+        //     from: ['/docs/guides/Public_Cloud_Deployment/Services']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/cloud/router',
+        //     from: ['/docs/guides/Public_Cloud_Deployment/Router']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/cloud/controller',
+        //     from: ['/docs/guides/Public_Cloud_Deployment/Controller']
+        //   },
+        //   {
+        //     to: '/docs/guides/topologies/gateway/tunneler',
+        //     from: ['/docs/guides/Local_Gateway/EdgeTunnel']
+        //   },
+        //   {
+        //     to: '/docs/guides/topologies/gateway/router',
+        //     from: ['/docs/guides/Local_Gateway/EdgeRouter']
+        //   },
+        //   {
+        //     to: '/docs/guides/deployments/linux/controller/backup',
+        //     from: ['/docs/guides/database-backup', '/docs/reference/backup/controller']
+        //   },
+        //   {
+        //     to: '/docs/reference/tunnelers/nginx',
+        //     from: ['/docs/category/securing-apis']
+        //   },
+        //   {
+        //     to: '/docs/category/cloud',
+        //     from: ['/docs/category/public-cloud-deployment']
+        //   },
+        //   {
+        //     to: '/docs/category/gateway',
+        //     from: ['/docs/category/local-gateway']
+        //   },
+        //   {
+        //     to: '/docs/category/kubernetes',
+        //     from: ['/docs/category/hosting-openziti']
+        //   },
+        //   {
+        //     to: '/blog/',
+        //     from: ['/blog/zitification/prometheus/part1']
+        //   },
+        //   {
+        //     to: '/docs/learn/core-concepts/data-flow-explainer',
+        //     from: ['/docs/guides/data-flow-explainer'],
+        //   },
+        //   {
+        //     to: '/docs/learn/core-concepts/metrics/overview',
+        //     from: ['/docs/core-concepts/metrics', '/docs/core-concepts/metrics/metric-types'],
+        //   },
+        //   {
+        //     to: '/docs/learn/core-concepts/security/authentication/external-jwt-signers',
+        //     from: ['/ziti/security/authentication/external-jwt-signers.html'],
+        //   },
+        //   {
+        //     to: '/docs/learn/core-concepts/identities/enrolling',
+        //     from: ['/ziti/identities/enrolling.html'],
+        //   },
+        //   {
+        //     to: '/docs/reference/tunnelers/',
+        //     from: ['/ziti/clients/tunneler.html'],
+        //   },
+        //   {
+        //     to: '/docs/reference/tunnelers/linux/',
+        //     from: ['/ziti/clients/linux.html'],
+        //   },
+        //   {
+        //     to: '/docs/learn/core-concepts/pki',
+        //     from: ['/docs/manage/pki', '/operations/pki'],
+        //   },
+        //   {
+        //     to: '/docs/reference/developer/sdk',
+        //     from: ['/api/ziti-sdk-swift', '/api/ziti-c-sdk', '/api/ziti-sdk-csharp'],
+        //   },
+        //   {
+        //     to: '/docs/reference/developer/sdk',
+        //     from: ['/docs/core-concepts/clients/sdks'],
+        //   },
+        //   {
+        //     to: '/docs/learn/introduction/',
+        //     from: ['/docs', '/docs/learn', '/docs/introduction/intro', '/docusaurus/docs/overview', '/ziti/overview/', '/ziti/overview.html'],
+        //   },
+        //   {
+        //     to: '/docs/learn/quickstarts/',
+        //     from: ['/ziti/quickstarts/quickstart-overview.html', '/ziti/quickstarts/networks-overview.html', '/docs/learn/quickstarts/network', '/docs/quickstarts/network'],
+        //   },
+        //   {
+        //     to: '/docs/learn/introduction/openziti-is-software',
+        //     from: ['/docs/introduction/zitiSoftwareArchitecture', '/ziti/software-architecture.html'],
+        //   },
+        //   {
+        //     to: '/policies/CODE_OF_CONDUCT',
+        //     from: ['/policies'],
+        //   },
+        //   {
+        //     to: '/docs/learn/quickstarts/services/',
+        //     from: ['/docs/quickstarts/services/ztna'],
+        //   },
+        //   {
+        //     to: '/docs/learn/quickstarts/zac/',
+        //     from: ['/docs/quickstarts/zac/installation'],
+        //   },
+        //   {
+        //     to: '/docs/guides/external-auth/browzer/',
+        //     from: ['/docs/identity-providers-for-browZer']
+        //   }
+        // ],
+      },
+>>>>>>> 2eaaf480 (start organizing deployment docs):docusaurus/docusaurus.config.js
     ],
   ],
   presets: [
