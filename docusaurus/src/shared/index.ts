fc35eab366cbc48f051a90e3d89c005826f17b1f
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-08-06 14:22:40 -0400

    update defaults to something else

--- docusaurus/src/shared/index.ts
+++ docusaurus/src/shared/index.ts
@@ -1,7 +1,7 @@
 export const DOCUSAURUS_DEBUG = process.env.DOCUSAURUS_DEBUG === 'true';
 export const DOCUSAURUS_URL = process.env.DOCUSAURUS_URL || 'https://openziti.io';
-export const DOCUSAURUS_BASE_PATH = process.env.DOCUSAURUS_BASE_PATH || '/DOCUSAURUS_BASE_PATH-unset';
-export const DOCUSAURUS_DOCS_PATH = process.env.DOCUSAURUS_DOCS_PATH || '/DOCUSAURUS_DOCS_PATH-unset';
+export const DOCUSAURUS_BASE_PATH = process.env.DOCUSAURUS_BASE_PATH || '/base-url';
+export const DOCUSAURUS_DOCS_PATH = process.env.DOCUSAURUS_DOCS_PATH || '/docs-path';
 export const hotjarId = process.env.ZITI_HOTJAR_APPID || "6443327"; //default localdev hotjarId
 
 export function cleanUrl(path:string) {
