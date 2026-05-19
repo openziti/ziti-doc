9ac2a1f9f303baea6f36ec7802a70680472c4d56
Nico Alba <nico.alba@netfoundry.io>
2026-05-19 18:42:59 +0000

    revert llocal  overides

--- docusaurus/src/theme/ScalarDocusaurus/index.tsx
+++ docusaurus/src/theme/ScalarDocusaurus/index.tsx
@@ -17,14 +17,6 @@ export const ScalarDocusaurus = ({ route }: Props) => {
     const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const stop = (e: KeyboardEvent) => {
-            if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'k') e.stopPropagation();
-        };
-        window.addEventListener('keydown', stop, true);
-        return () => window.removeEventListener('keydown', stop, true);
-    }, []);
-
-    useEffect(() => {
         if (!window.Scalar || !ref.current) return;
         const instance = window.Scalar.createApiReference(ref.current, {
             ...route.configuration,
